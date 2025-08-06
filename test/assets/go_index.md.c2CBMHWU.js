56ba0384a6abbdd15a01eb1d502cb2f2be72e38b
raino_tx <2656537241@qq.tx.com>
2025-08-07 00:38:31 +0800

    test-deploy

---
+++ test/assets/go_index.md.c2CBMHWU.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o as a}from"./chunks/framework.BEuPqn_A.js";const m=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"go/index.md","filePath":"go/index.md","lastUpdated":1754492921000}'),o={name:"go/index.md"};function n(r,s,d,c,i,p){return a(),t("div")}const f=e(o,[["render",n]]);export{m as __pageData,f as default};
